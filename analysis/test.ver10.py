c7b69a8d70b89e82c9f2f6e82ab12a7f153ea500
tmyuu <tmyuu@proton.me>
2023-12-11 20:53:35 +0900

    final submit

--- analysis/test.ver10.py
+++ analysis/test.ver10.py
@@ -15,22 +15,15 @@ def solution(x_test_df, train_df):
     import tensorflow as tf
     from sklearn.utils.class_weight import compute_class_weight
     from PIL import Image
-    from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Add
+    from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Add
     from tensorflow.keras.models import Model
 
     def resize_map(map):
 
         resize_shape = (32, 32)
 
-        len_y, len_x = map.shape
-    
-        y_add = len_y // 2 + len_y % 2
-        x_add = len_x // 2 + len_x % 2
-    
-        y_indices, x_indices = np.where(map == 0)
-        map[y_indices, x_indices] = map[(y_indices + y_add) % len_y, (x_indices + x_add) % len_x]
-        y_indices, x_indices = np.where(map == 0)
-        map[y_indices, x_indices] = 1
+        non_zero_average = np.mean(map[map != 0])
+        map[map == 0] = non_zero_average
     
         map = Image.fromarray(map - 1.0)
         resized_map = map.resize(resize_shape, Image.LANCZOS)
@@ -61,17 +54,18 @@ def solution(x_test_df, train_df):
     
         x = Conv2D(16, 3, activation='relu', padding='same')(inputs)
         x = MaxPooling2D(pool_size=2, strides=2)(x)
-        skip_connection = Conv2D(64, 1, strides=2, activation='relu', padding='same')(x)
+        skip_connection = Conv2D(64, 3, strides=2, activation='relu', padding='same')(x)
 
         x = Conv2D(64, 3, activation='relu', padding='same')(x)
         x = MaxPooling2D(pool_size=2, strides=2)(x)
         x = Add()([x, skip_connection])
 
-        x = Conv2D(128, 3, activation='relu', padding='same')(x)
+        x = Conv2D(128, 1, activation='relu', padding='same')(x)
         x = MaxPooling2D(pool_size=2, strides=2)(x)
     
         x = Flatten()(x)
         x = Dense(256, activation='relu')(x)
+        x = Dropout(0.1)(x)
     
         outputs = Dense(failure_types_classes)(x)
     
@@ -100,7 +94,7 @@ def solution(x_test_df, train_df):
     class_weights = calculate_class_weights(train_labels)
 
     model = initialize_cnn(input_shape, failure_types_classes)
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.00024),
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.00020),
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
     model.fit(train_maps, train_labels, epochs=10, batch_size=64, class_weight=class_weights)
@@ -118,58 +112,33 @@ def solution(x_test_df, train_df):
     
     return pd.DataFrame({'failureType': answer}, index=x_test_df.index)
 
-def plot_confusion_matrix_and_accuracy(y_true, y_pred, classes):
-    import seaborn as sns
-    import matplotlib.pyplot as plt
-    from sklearn.metrics import confusion_matrix
-
-    # Confusion matrixの計算
-    cm = confusion_matrix(y_true, y_pred, labels=classes)
-
-    # ヒートマップとしてプロット
-    plt.figure(figsize=(10, 10))
-    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=classes, yticklabels=classes)
-    plt.title('Confusion Matrix')
-    plt.ylabel('Actual')
-    plt.xlabel('Predicted')
-    plt.show()
-
-    # 各クラスごとの正確さと最も間違えやすいクラスを表示
-    print("\nClass Accuracy and Most Common Errors:")
-    for i, class_name in enumerate(classes):
-        accuracy = cm[i, i] / cm[i, :].sum()
-        print(f"{class_name}: Accuracy: {accuracy * 100:.2f}%")
-        
-        # 最も間違えやすいクラスを特定
-        error_indices = cm[i, :].argsort()[-2:-1] if accuracy < 1 else []
-        for error_index in error_indices:
-            error_rate = cm[i, error_index] / cm[i, :].sum()
-            error_class = classes[error_index]
-            print(f"    Most common error: Mistaken for {error_class} ({error_rate * 100:.2f}%)")
-
 # データのインポート
-df=pd.read_pickle("../work/input/LSWMD_25519.pkl")
+df = pd.read_pickle("../work/input/LSWMD_25519.pkl")
 
-# テスト用と学習用のデータを作成（テストする際は、random_stateの値などを編集してみてください）
+# テスト用と学習用のデータを作成
 train_df, test_df = train_test_split(df, stratify=df['failureType'], test_size=0.10, random_state=42)
 
 y_test_df = test_df[['failureType']]
 x_test_df = test_df.drop(columns=['failureType'])
 
-# solution関数を実行
-user_result_df = solution(x_test_df, train_df)
-plot_confusion_matrix_and_accuracy(y_test_df['failureType'], user_result_df['failureType'], df['failureType'].unique())
+# 10回の繰り返しでsolution関数を実行し、平均精度を計算
+average_accuracies = []
+for i in range(10):
+    # solution関数を実行
+    user_result_df = solution(x_test_df, train_df)
 
-average_accuracy = 0
-# ユーザーの提出物のフォーマット確認
-if type(y_test_df) == type(user_result_df) and y_test_df.shape == user_result_df.shape:
     # 平均精度の計算
-    accuracies = {}
-    for failure_type in df['failureType'].unique():
-        y_test_df_by_failure_type = y_test_df[y_test_df['failureType'] == failure_type]
-        user_result_df_by_failure_type = user_result_df[y_test_df['failureType'] == failure_type]
-        matching_rows = (y_test_df_by_failure_type == user_result_df_by_failure_type).all(axis=1).sum()
-        accuracies[failure_type] = (matching_rows/(len(y_test_df_by_failure_type)))
-    
-    average_accuracy = sum(accuracies.values())/len(accuracies)
-print(f"平均精度：{average_accuracy*100:.2f}%")
+    if type(y_test_df) == type(user_result_df) and y_test_df.shape == user_result_df.shape:
+        accuracies = {}
+        for failure_type in df['failureType'].unique():
+            y_test_df_by_failure_type = y_test_df[y_test_df['failureType'] == failure_type]
+            user_result_df_by_failure_type = user_result_df[y_test_df['failureType'] == failure_type]
+            matching_rows = (y_test_df_by_failure_type == user_result_df_by_failure_type).all(axis=1).sum()
+            accuracies[failure_type] = (matching_rows / len(y_test_df_by_failure_type))
+
+        average_accuracy = sum(accuracies.values()) / len(accuracies)
+        average_accuracies.append(average_accuracy)
+
+# 10回の繰り返しの平均精度を計算
+final_average_accuracy = sum(average_accuracies) / len(average_accuracies)
+print(f"10回の平均精度：{final_average_accuracy * 100:.2f}%")
--- analysis/test.ver3.py
+++ analysis/test.ver3.py
@@ -1,3 +1,13 @@
+import numpy as np # https://numpy.org/ja/
+import pandas as pd # https://pandas.pydata.org/
+from sklearn.model_selection import train_test_split
+
+# !pip install numpy
+# !pip install pandas
+# !pip install scikit-learn
+# !pip install tensorflow
+# !pip install Pillow
+
 def normalize_map(map):
     from PIL import Image
 
@@ -28,27 +38,36 @@ def normalize_map(map):
     return np.asarray(resized_map)
 
 
-def preprocess_map(df, normalize_map):
+def preprocess_map1(x_test_df, normalize_map):
     # データの正規化
-    train_maps = np.array([normalize_map(x) for x in df['waferMap']])
+    train_maps = np.array([normalize_map(x) for x in x_test_df['waferMap']])
+    # データの形状を変更
+    train_maps = train_maps.reshape(train_maps.shape + (1,))
+
+    return train_maps
+
+
+def preprocess_map2(train_df, normalize_map):
+    # データの正規化
+    train_maps = np.array([normalize_map(x) for x in train_df['waferMap']])
 
     # 1. 画像を水平方向に反転
     flipped_horizontally = np.flip(train_maps, axis=2)
     train_maps = np.concatenate((train_maps, flipped_horizontally), axis=0)
 
-    # # 2. 画像を垂直方向に反転
+    # 2. 画像を垂直方向に反転
     # flipped_vertically = np.flip(train_maps, axis=1)
     # train_maps = np.concatenate((train_maps, flipped_vertically), axis=0)
 
-    # 3. 画像を90度回転
-    rotated_90 = np.rot90(train_maps, k=1, axes=(1, 2))
-    train_maps = np.concatenate((train_maps, rotated_90), axis=0)
-
-    # 4. 画像を180度回転
+    # 3. 画像を180度回転
     rotated_180 = np.rot90(train_maps, k=2, axes=(1, 2))
     train_maps = np.concatenate((train_maps, rotated_180), axis=0)
 
-    # # 5. 画像を270度回転
+    # 4. 画像を90度回転
+    rotated_90 = np.rot90(train_maps, k=1, axes=(1, 2))
+    train_maps = np.concatenate((train_maps, rotated_90), axis=0)
+
+    # 5. 画像を270度回転
     # rotated_270 = np.rot90(train_maps, k=3, axes=(1, 2))
     # train_maps = np.concatenate((train_maps, rotated_270), axis=0)
 
@@ -58,57 +77,33 @@ def preprocess_map(df, normalize_map):
     return train_maps
 
 
-def preprocess_size(df):
+def create_model(input_shape, num_classes):
     import tensorflow as tf
 
-    # dieSizeの正規化（0-1の間にスケーリング）
-    size = df['dieSize'].values
-    size = size / tf.reduce_max(size)
+    model = tf.keras.models.Sequential([
+        # 畳み込みブロック1
+        tf.keras.layers.Conv2D(24, 3, activation='relu', padding='same', input_shape=(input_shape)),
+        tf.keras.layers.MaxPooling2D(pool_size=2, padding='same'),
+        skip_connection = Conv2D(64, 1, strides=2, activation='relu', padding='same')(x)
+        
+        # 畳み込みブロック2
+        tf.keras.layers.Conv2D(24, 3, activation='relu', padding='same'),
+        tf.keras.layers.MaxPooling2D(pool_size=2),
+        x = Add()([x, skip_connection])
+        
+        # 畳み込みブロック3
+        tf.keras.layers.Conv2D(48, 3, activation='relu', padding='same'),
+        tf.keras.layers.MaxPooling2D(pool_size=2),
+        
+        # ブロック4
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(64, activation=tf.nn.relu),
+        tf.keras.layers.Dense(16, activation=tf.nn.relu),
 
-    return size
+        # 出力層
+        tf.keras.layers.Dense(num_classes),
+    ])
 
-
-def create_hybrid_model(map_shape, num_classes):
-    import tensorflow as tf
-    from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout, concatenate, Multiply
-    from tensorflow.keras.models import Model
-
-    # waferMap用の入力レイヤー
-    wafermap_input = Input(shape=map_shape, name='wafermap_input')
-    
-    # 畳み込みとプーリング層
-    x = Conv2D(16, (3, 3), activation='relu', padding='same')(wafermap_input)
-    x = MaxPooling2D(pool_size=(2, 2), padding='same')(x)
-    x = Conv2D(40, (3, 3), activation='relu', padding='same')(x)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-    x = Conv2D(48, (3, 3), activation='relu', padding='same')(x)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-    wafermap_features = Flatten()(x)
-
-    # dieSize用の入力レイヤー
-    diesize_input = Input(shape=(1,), name='diesize_input')
-    diesize_dense = Dense(16, activation='relu')(diesize_input)
-
-    # 注意機構の適用
-    # `wafermap_features`の長さに合わせて`attention_probs`を変換
-    attention_dense = Dense(tf.keras.backend.int_shape(wafermap_features)[-1], activation='softmax', name='attention_vec')(diesize_dense)
-    attention_mul = Multiply()([wafermap_features, attention_dense])
-
-    # 特徴量の結合
-    combined_features = concatenate([attention_mul, diesize_dense])
-
-    # 密結合層とドロップアウト
-    x = Dense(320, activation='relu')(combined_features)
-    x = Dropout(0.1)(x)
-    x = Dense(80, activation='relu')(x)
-    x = Dropout(0.1)(x)
-
-    # 出力層
-    outputs = Dense(num_classes)(x)
-
-    # モデルの定義
-    model = Model(inputs=[wafermap_input, diesize_input], outputs=outputs)
-    
     return model
 
 
@@ -118,49 +113,61 @@ def calculate_class_weights(train_labels):
     class_weights = compute_class_weight(class_weight='balanced', 
                                          classes=np.unique(train_labels), 
                                          y=train_labels)
-    # class_weights[4] *= 1.5
     # クラスの重みを辞書型に変換
     return dict(enumerate(class_weights))
 
-
 def solution(x_test_df, train_df):
-    import os
-    os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
     import tensorflow as tf
-    #import keras_tuner as kt
-
-    # failureTypeのユニークな値を取得
     failure_types = list(train_df['failureType'].unique())
 
-    test_maps = preprocess_map(x_test_df, normalize_map)
-    train_maps = preprocess_map(train_df, normalize_map)
-    test_sizes = np.repeat(preprocess_size(x_test_df), 8)
-    train_sizes = np.repeat(preprocess_size(train_df), 8)
+    # 前処理
+    # test_maps = preprocess_map1(x_test_df, normalize_map)
+    test_maps = preprocess_map2(x_test_df, normalize_map)
+    train_maps = preprocess_map2(train_df, normalize_map)
     train_labels = np.array([failure_types.index(x) for x in train_df['failureType']] * 8)
-
+    
+    # クラスの重みを計算
     class_weights = calculate_class_weights(train_labels)
 
-    model = create_hybrid_model(train_maps[0].shape, len(failure_types))
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    model.fit([train_maps, train_sizes], train_labels, epochs=5, class_weight=class_weights)
+    # モデルの作成
+    model = create_model(train_maps[0].shape, len(failure_types))
+    model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+    model.fit(train_maps, train_labels, epochs=15, batch_size=64, class_weight=class_weights)
 
-    # tuner = kt.RandomSearch(
-    #     create_model,
-    #     objective='val_accuracy',
-    #     max_trials=10,
-    #     directory='tuner',
-    #     project_name='wafermap'
-    # )
-
-    # tuner.search(train_maps, train_labels, epochs=10, validation_split=0.1)
-
-    # best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
-    # model = tuner.hypermodel.build(best_hps)
-    # model.fit(train_maps, train_labels, epochs=10, class_weight=class_weights)
-
-    # 各予測結果の平均を計算
-    test_logits = np.mean(model.predict([test_maps, test_sizes]).reshape(-1, len(x_test_df['waferMap']), len(failure_types)), axis=0)
+    test_len = len(x_test_df['waferMap'])
+    # 予測
+    logits = model.predict(test_maps)
+    test_logits = np.zeros(test_len * len(failure_types), dtype=np.float64).reshape((test_len,len(failure_types)))
+    for n in range(8):
+        test_logits += logits[test_len*n:test_len*(n + 1)]
+    
     predictions = tf.nn.softmax(test_logits).numpy()
     answer = [failure_types[x.argmax()] for x in predictions]
 
     return pd.DataFrame({'failureType': answer}, index=x_test_df.index)
+
+# データのインポート
+df=pd.read_pickle("../work/input/LSWMD_25519.pkl")
+
+# テスト用と学習用のデータを作成（テストする際は、random_stateの値などを編集してみてください）
+train_df, test_df = train_test_split(df, stratify=df['failureType'], test_size=0.10, random_state=42)
+
+y_test_df = test_df[['failureType']]
+x_test_df = test_df.drop(columns=['failureType'])
+
+# solution関数を実行
+user_result_df = solution(x_test_df, train_df)
+
+average_accuracy = 0
+# ユーザーの提出物のフォーマット確認
+if type(y_test_df) == type(user_result_df) and y_test_df.shape == user_result_df.shape:
+    # 平均精度の計算
+    accuracies = {}
+    for failure_type in df['failureType'].unique():
+        y_test_df_by_failure_type = y_test_df[y_test_df['failureType'] == failure_type]
+        user_result_df_by_failure_type = user_result_df[y_test_df['failureType'] == failure_type]
+        matching_rows = (y_test_df_by_failure_type == user_result_df_by_failure_type).all(axis=1).sum()
+        accuracies[failure_type] = (matching_rows/(len(y_test_df_by_failure_type)))
+    
+    average_accuracy = sum(accuracies.values())/len(accuracies)
+print(f"平均精度：{average_accuracy*100:.2f}%")
